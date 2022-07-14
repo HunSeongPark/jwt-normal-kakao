03366694791683983365bfaa5ef34e012dd729ba
Hunseong <phunseong@gmail.com>
2022-07-14 14:43:29 +0900

    initial commit

---
+++ src/main/java/com/hunseong/jwtkakao/JwtKakaoApplication.java
@@ -0,0 +1,13 @@
+package com.hunseong.jwtkakao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtKakaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtKakaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hunseong/jwtkakao/JwtKakaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hunseong.jwtkakao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtKakaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
